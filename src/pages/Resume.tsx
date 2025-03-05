98628b9f8366a5ff298c58a2bbd78a48363ca288
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2025-03-05 15:26:57 -0500

    updated resume link

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -16,7 +16,7 @@ const Resume = () => {
       <div className="flex-grow pt-20">
         <div className="container mx-auto px-4">
           <iframe
-            src="/images/resume/resume.pdf"
+            src="/images/resume/Resume_CS.pdf"
             className="w-full h-[calc(100vh-120px)]"
             title="Resume PDF"
           />
