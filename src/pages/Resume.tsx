bd232c72e2343d7ccc4668013d58e0c88f369737
Jean Pascal Cyusa Shyaka <shyakaadolf@gmail.com>
2025-03-11 13:00:40 -0400

    updated resume link as a box embed

--- src/pages/Resume.tsx
+++ src/pages/Resume.tsx
@@ -1,24 +1,17 @@
 import Navigation from "@/components/Navigation";
 import Footer from "@/components/Footer";
-import { useEffect } from 'react';
-
-// declare global {
-//   interface Window {
-//     AdobeDC: AdobeDC;
-//   }
-// }
 
 const Resume = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navigation />
-      {/* Added pt-20 to create space below navigation */}
       <div className="flex-grow pt-20">
         <div className="container mx-auto px-4">
           <iframe
-            src="/images/resume/Resume_CS.pdf"
-            className="w-full h-[calc(100vh-120px)]"
+            src="https://tufts.box.com/v/pascal-resume"
+            className="w-full h-[calc(100vh-120px)] border-0"
             title="Resume PDF"
+            allowFullScreen
           />
         </div>
       </div>
